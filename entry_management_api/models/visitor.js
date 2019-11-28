921ad4ca9d247c8789ca183442e3229d24aa956d
thesid01 <35187359+thesid01@users.noreply.github.com>
2019-11-28 14:33:55 +0530

    Update visitor.js

--- entry_management_api/models/visitor.js
+++ entry_management_api/models/visitor.js
@@ -88,8 +88,8 @@ function sendSMS(data,callback){
       error = err;
     }else{
       const nexmo = new Nexmo({
-        apiKey: '49851512',
-        apiSecret: 'KOFBVNHQ1KOqRw4Q',
+        apiKey: config.nexmoInfo.API_KEY,
+        apiSecret: config.nexmo.SECRET_KEY,
       }, {debug:true});
 
       const from = data.name;
