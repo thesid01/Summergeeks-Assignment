71ed411695e3659dd09a48a7b76ca7e9238b8c91
thesid01 <35187359+thesid01@users.noreply.github.com>
2019-11-28 14:31:44 +0530

    Update config.js

--- entry_management_api/config/config.js
+++ entry_management_api/config/config.js
@@ -2,5 +2,10 @@ var mailInfo = {
     username : 'xxxx@xxxxxxx.com' ,
     password : 'xxxxxxxxx' 
 }
+var nexmoInfo = {
+    API_KEY: 'xxxxxxxxx',
+    SECRET_KEY: 'xxxxxxxxxx',
+}
 
+module.exports.nexmoInfo = nexmoInfo;
 module.exports.mailInfo = mailInfo;
